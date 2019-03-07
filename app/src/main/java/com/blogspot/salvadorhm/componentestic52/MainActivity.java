752783e04102d4a96d0d2d16cf5908fffc5dd6e1
Salvador Hernández Mendoza <salvadorhm@utec-tgo.edu.mx>
2019-03-07 08:30:51 -0600

    Actualización del proyecto

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/MainActivity.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/MainActivity.java
@@ -1,5 +1,6 @@
 package com.blogspot.salvadorhm.componentestic52;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -91,6 +92,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        }else if(id == R.id.nav_edit_text){
 
         }
 
