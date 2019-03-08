6b7c1cb103fa482ca8110604762f9b53aa107148
Sury Sarahi Trejo Espindola <1717110260@utectulancingo.edu.mx>
2019-03-08 17:56:09 -0600

    El ScrollView y HorizontalScrollView fue exitoso

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Scroll_View_Horizontal_Scoll_View.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic52;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Scroll_View_Horizontal_Scoll_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__scroll__view__horizontal__scoll__view);
+    }
+}
