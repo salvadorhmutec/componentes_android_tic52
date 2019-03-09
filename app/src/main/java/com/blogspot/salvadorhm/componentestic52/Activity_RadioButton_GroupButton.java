940e7d69a2b5277e0971a22411584d3d36c508e7
Mayra Patricia Perez Martinez <1717110752@utectulancingo.edu.mx>
2019-03-09 14:24:44 -0600

    Activity RadioButton_GroupButton Creada

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_RadioButton_GroupButton.java
@@ -0,0 +1,13 @@
+xpackage com.blogspot.salvadorhm.componentestic52;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_RadioButton_GroupButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__radio_button__group_button);
+    }
+}
