50ff8ef17ed563e2354291c64b9e87064bf63191
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2019-03-11 05:41:38 -0600

    Actividad web_view creada

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic52;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Web_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__web__view);
+    }
+}
