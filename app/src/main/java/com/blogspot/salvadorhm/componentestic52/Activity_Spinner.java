8164e273c032d98893a8472a6bbd7863777aee27
abner joel lira silva <1717110622@utectulancingo.edu.mx>
2019-03-13 07:29:02 -0600

    activity spinner realizada

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Spinner.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic52;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Spinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__spinner);
+    }
+}
