556a5e15d504e1e24fb7b9225b8d5714d4ff7356
Salvador Hernández Mendoza <salvadorhm@utec-tgo.edu.mx>
2019-03-13 23:45:01 -0600

    Proyecto depurado, MainActivity y menu comprobado y funcionando.

--- README.md
+++ README.md
@@ -4,21 +4,21 @@ Aplicación desarrollada en AndroidStudio 3.3.1 con una demostración del uso de
 
 ## Lista de componentes
 
-1. ScrollView y HorizontalScrollView - Sury Sarahi Trejo Espindola
-2. EditText - Jocelyn Flores Badillo
-3. Button Jonatan Vazquez Tellez
-4. ImageButton - Juan Pablo Duran Rendon
-5. CheckBox - Manuel Alonso 
-6. RadioGrup y RadioButtton - Mayra Patricia Pérez Martínez
-7. ToggleButton - Luis Norberto Paloma Rodriguez
-8. Switch -Yoselin Ortiz Romero
+1. ScrollView y HorizontalScrollView - Sury Sarahi Trejo Espindola - Activity
+2. EditText - Jocelyn Flores Badillo - Activity
+3. Button Jonatan Vazquez Tellez - Activity
+4. ImageButton - Juan Pablo Duran Rendon - Activity
+5. CheckBox - Manuel Alonso  - Activity
+6. RadioGrup y RadioButtton - Mayra Patricia Pérez Martínez - Activity
+7. ToggleButton - Luis Norberto Paloma Rodriguez - Activity
+8. Switch -Yoselin Ortiz Romero - Activity
 9. FloatingActionButton - Jose Luis Aparicio Guzman
-10. ImageView - Oscar Escamilla Luqueño 
-11. WebView - Cristian Gayosso Peralta
-12. VideoView - Daniela Rubiales Marquez
-13. CalendarView - Esteban Islas Santos
-14. RatingBar - Daniela Garcia Rios
-15. ProgressBar -Gabriela Curiel Garcia
-16. TabLayout - Sebastián de Jesús Alfaro Rosales
-17. Spinner abner joel lira silva
-18. MapView - Isais Manuel Aranda Leal
+10. ImageView - Oscar Escamilla Luqueño - Activity
+11. WebView - Cristian Gayosso Peralta - Activity
+12. VideoView - Daniela Rubiales Marquez - Activity
+13. CalendarView - Esteban Islas Santos - 
+14. RatingBar - Daniela Garcia Rios - Activity
+15. ProgressBar -Gabriela Curiel Garcia - Activity
+16. TabLayout - Sebastián de Jesús Alfaro Rosales - Activity
+17. Spinner abner joel lira silva - Activity
+18. MapView - Isais Manuel Aranda Leal - Activity
--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/MainActivity.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/MainActivity.java
@@ -2,6 +2,7 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -21,10 +22,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,19 +34,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -77,28 +78,68 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_button) {
-            Intent intent = new Intent(getApplicationContext(), Activity_Button.class);
+        if (id == R.id.nav_edit_text) {
+            Intent intent = new Intent(getApplicationContext(),Activity__Edit_Text.class);
             startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        } else if (id == R.id.nav_button) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Button.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_check_box) {
+            Intent intent = new Intent(getApplicationContext(),Activity_CheckBox.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_image_button) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Image_Button.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_image_view) {
+            Intent intent = new Intent(getApplicationContext(),Activity_image_view.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_calendar_view) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Calendar_View.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_map_view) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Map_View.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_progress_bar) {
+            Intent intent = new Intent(getApplicationContext(),Activity_ProgressBar.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_radio_button) {
+            Intent intent = new Intent(getApplicationContext(),Activity_RadioButton_GroupButton.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_rating_bar) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Rating_Bar.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_scroll_view) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Scroll_View_Horizontal_Scoll_View.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_spinner) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Spinner.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_switch) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Switch.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_tab_layout) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Tab_Layout.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_toggle_button) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Toggle_Button.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_web_view) {
+            Intent intent = new Intent(getApplicationContext(), Activity_Web_View.class);
+            startActivity(intent);
+        }else if (id == R.id.nav_video_view) {
+            Intent intent = new Intent(getApplicationContext(), VideoView.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
-        }else if(id == R.id.nav_edit_text){
-
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
