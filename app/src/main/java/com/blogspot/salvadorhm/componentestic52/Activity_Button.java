c13d6a07947a1166e0612f2dd07ceb48b77d61b7
jonatan <jonatan@gmail.com>
2019-03-17 00:01:52 -0600

    Button realizado exitosamente

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Button.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Button.java
@@ -2,12 +2,29 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Activity_Button extends AppCompatActivity {
-
+public class Activity_Button extends AppCompatActivity implements View.OnClickListener {
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__button);
+        boton =(Button) findViewById(R.id.button10);
+
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button10:
+                Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+                    break;
+        }
     }
 }
