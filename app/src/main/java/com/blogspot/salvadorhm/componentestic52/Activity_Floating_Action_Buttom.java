316ec1eec8644305277bce70b997e8f129cec9c4
Wicho2715 <1717110268@utectulancingo.edu.mx>
2019-03-17 10:25:50 -0600

    creacion de floatingActionMenu y FloatingActionButton

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Floating_Action_Buttom.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Floating_Action_Buttom.java
@@ -2,12 +2,27 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionMenu;
 
 public class Activity_Floating_Action_Buttom extends AppCompatActivity {
+    FloatingActionMenu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__floating__action__buttom);
+
+        actionMenu=(FloatingActionMenu)findViewById(R.id.fabPrincipal);
+        actionMenu.setClosedOnTouchOutside(true);
+    }
+    public void clicSubMenu1(View view){
+        Toast.makeText(this, "Sub Menu 1 Tocado",Toast.LENGTH_SHORT).show();
+    }
+    public void clicSubMenu2(View view){
+        Toast.makeText(this, "Sub Menu 2 Tocado",Toast.LENGTH_SHORT).show();
     }
 }
+
