c6afc7f15256edd5c59aade3f708c50feee43e50
abner joel lira silva <1717110622@utectulancingo.edu.mx>
2019-03-18 10:55:59 -0600

    error reparado

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Spinner.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Spinner.java
@@ -13,7 +13,7 @@ public class Activity_Spinner extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity__spinner);
 
         opciones =(Spinner) findViewById(R.id.sp01);
 
