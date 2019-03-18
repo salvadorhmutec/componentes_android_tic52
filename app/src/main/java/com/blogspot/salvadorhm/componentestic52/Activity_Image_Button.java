ee3a070494cd3477d3f72aa94c401fa622a0cd27
Juan Pablo Durán Rendón <jpablodr11@gmail.com>
2019-03-18 14:57:00 -0600

    Actividad ImageButton Terminada

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Image_Button.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Image_Button.java
@@ -2,12 +2,19 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity_Image_Button extends AppCompatActivity {
-
+    private TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__image__button);
+        texto= (TextView)findViewById(R.id.texto);
+    }
+    public void Info(View view){
+       texto.setText("El Image Button es un botón con mejor estética y puede ser una imagen, básicamente es una imagen que realiza un evento(acción) en concreto");
     }
 }
