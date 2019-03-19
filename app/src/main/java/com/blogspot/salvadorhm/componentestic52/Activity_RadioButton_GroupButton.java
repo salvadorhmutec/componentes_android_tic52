f095d54ffe7d69e5f5a3c98e8cadd9537aa64e1e
PatyPrz <1717110752@utectulancingo.edu.mx>
2019-03-18 23:15:02 -0600

    Programacion de las actividades para activity RadioButton

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_RadioButton_GroupButton.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_RadioButton_GroupButton.java
@@ -2,6 +2,9 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Activity_RadioButton_GroupButton extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class Activity_RadioButton_GroupButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__radio_button__group_button);
+        RadioButton radioButton_Piratas = (RadioButton) findViewById(R.id.radio_pirates);
+        RadioButton radioButton_Ninjas = (RadioButton) findViewById(R.id.radio_ninjas);
+    }
+
+    public void onRadioButtonClicked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.radio_pirates:
+                if(checked){
+                    Toast.makeText(this,"Los piratas son los mejores!",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.radio_ninjas:
+                if(checked){
+                    Toast.makeText(this,"Los ninjas son los mejores!",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
     }
 }
