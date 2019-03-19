e60d136ff18f8221b6bfbf1faa703579db97f68e
DanielaRubiales <>
2019-03-18 22:24:16 -0600

    Programacion de las actividades del activity video view

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/VideoView.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/VideoView.java
@@ -1,7 +1,11 @@
 package com.blogspot.salvadorhm.componentestic52;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+
+import static com.blogspot.salvadorhm.componentestic52.R.*;
 
 public class VideoView extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class VideoView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_view);
+
+        android.widget.VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
