9209437ea820b6ce58d0329255fb27dcd9cd192b
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2019-03-20 08:59:25 -0600

    webview terminado

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
@@ -1,28 +1,31 @@
 package com.blogspot.salvadorhm.componentestic52;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
 
 public class Activity_Web_View extends AppCompatActivity {
-    String url = "https://www.google.com.mx/";
+
+    WebView browser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__web__view);
+        setContentView(R.layout.activity_main);
 
-        WebView web = findViewById(R.id.web_view);
-            web.setWebViewClient(new MyWebViewClient());
-             WebSettings settings = web.getSettings();
-             settings.setJavaScriptEnabled(true);
-             web.loadUrl(url);
-    }
-private class  MyWebViewClient extends WebViewClient{
-        public  boolean shouldOverrideUrlLoading(WebView view, String url){
-            view.loadUrl(url);
-            return true;
-        }
+        // Definimos el webView
+        browser= findViewById(R.id.web_view);
+
+        browser.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        // Cargamos la web
+        browser.loadUrl("file:///re/index.html");
     }
 }
