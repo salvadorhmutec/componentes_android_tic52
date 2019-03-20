5203025d5bae4d9dff4c9a2075d9026d1e9b0c25
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2019-03-20 08:03:31 -0600

    Actualizacion

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
@@ -2,7 +2,9 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Activity_Web_View extends AppCompatActivity {
     String url = "https://www.google.com.mx/";
@@ -11,6 +13,16 @@ public class Activity_Web_View extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__web__view);
 
-        WebView web =(WebView) findViewById(R.id.web_view);
+        WebView web = findViewById(R.id.web_view);
+            web.setWebViewClient(new MyWebViewClient());
+             WebSettings settings = web.getSettings();
+             settings.setJavaScriptEnabled(true);
+             web.loadUrl(url);
+    }
+private class  MyWebViewClient extends WebViewClient{
+        public  boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
