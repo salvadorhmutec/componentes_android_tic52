6b88db26df965c4248ccb4f65213df7080bcaae2
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2019-03-20 06:25:57 -0600

    avance_1

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Web_View.java
@@ -2,12 +2,15 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Activity_Web_View extends AppCompatActivity {
-
+    String url = "https://www.google.com.mx/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__web__view);
+
+        WebView web =(WebView) findViewById(R.id.web_view);
     }
 }
