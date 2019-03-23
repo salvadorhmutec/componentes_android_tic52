723e6fbfc2c4a159e046573b17ea1f614cd61a42
Yoselin Ortiz Romero <1717110661@utectulancingo.edu.mx>
2019-03-23 08:52:47 -0600

    Switch Funcionando

--- app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Switch.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic52/Activity_Switch.java
@@ -2,6 +2,9 @@ package com.blogspot.salvadorhm.componentestic52;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Activity_Switch extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Activity_Switch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__switch);
+        Switch switch1 =(Switch)findViewById(R.id.switch1);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(), "ON", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "OFF", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
